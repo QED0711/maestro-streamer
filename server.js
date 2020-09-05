ddc5fcb9776852db3f57e6f0ab7839797eff5b10
QED0711 <qdizon@gmail.com>
2020-09-05 05:33:33 -0400

    join session does a check to see what kind of session (streaming or chat) the user has joined

--- server.js
+++ server.js
@@ -11,10 +11,10 @@ const server = require("http").createServer(app)
 const io = require("socket.io")(server)
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
-  debug: true
+    debug: true
 });
 
-const {v4: uuidV4} = require("uuid");
+const { v4: uuidV4 } = require("uuid");
 
 
 // server configuration
@@ -22,20 +22,20 @@ app.use(cors())
 app.use('/peerjs', peerServer)
 app.set("view engine", "ejs")
 app.use(express.static('public'))
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
-  });
+});
 
 // ROUTES
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.redirect(`/${uuidV4()}`)
 })
 
-app.get("/:sessionID", (req,res) => {
+app.get("/:sessionID", (req, res) => {
     const part = req.query.part
-    res.render("session", {sessionID: req.params.sessionID, part})
+    res.render("session", { sessionID: req.params.sessionID, part })
 })
 
 // SOCKET IO
@@ -44,49 +44,54 @@ io.on("connection", socket => {
     // socket.on("hello", () => {
     //     socket.emit("world", {message:"This is a message from the server socket"})
     // })
-    socket.on('join-chat-session', ({sessionID}) => {
-        console.log("JOINED CHAT SESSION")
-        socket.join(sessionID)
-
-        socket.on("chat-message", ({name, message}) => {
-            socket.to(sessionID).broadcast.emit("message-received", {name, message})
-        })
-    })
     
-    socket.on("join-session", ({sessionID, userID, part}) => {
-        
+    // socket.on('join-chat-session', ({ sessionID }) => {
+    //     console.log("JOINED CHAT SESSION", sessionID)
+    //     socket.join(sessionID)
+
+    //     socket.on("chat-message", ({ name, message }) => {
+    //         socket.to(sessionID).broadcast.emit("message-received", { name, message })
+    //     })
+    // })
+
+    socket.on("join-session", ({ sessionID, userID, part }) => {
+
         socket.join(sessionID);
         // setTimeout(() => {
         //     socket.to(sessionID).broadcast.emit("user-connected", {userID, part})
         // }, 3000)
-        
-        socket.on("ring", ({sessionID, userID, authority}) => {
-            console.log({userID, sessionID, authority})
-            // for continuous ringing
-            socket.to(sessionID).broadcast.emit("user-connected", {userID, authority})
+
+        socket.on("chat-message", ({ name, message }) => {
+            socket.to(sessionID).broadcast.emit("message-received", { name, message })
         })
 
-        socket.on("chat-message", ({name, message}) => {
-            socket.to(sessionID).broadcast.emit("message-received", {name, message})
-        })
+        if (!!userID) { // if a user ID was sent, then they are joining the main stream session. If none sent, then they are just joining the chat session
 
-        socket.on("user-leaving", data => {
-            console.log("USER LEAVING")
-            socket.to(sessionID).broadcast.emit("remove-stale-user", {...data})
-        })
-
-        socket.on("disconnect", () => {
-            socket.to(sessionID).broadcast.emit("user-disconnected", userID)
-        })
+            socket.on("ring", ({ sessionID, userID, authority }) => {
+                console.log({ userID, sessionID, authority })
+                // for continuous ringing
+                socket.to(sessionID).broadcast.emit("user-connected", { userID, authority })
+            })
 
 
-        socket.on("request-user-data", data => {
-            socket.to(sessionID).broadcast.emit("data-requested", data)
-        })
+            socket.on("user-leaving", data => {
+                console.log("USER LEAVING")
+                socket.to(sessionID).broadcast.emit("remove-stale-user", { ...data })
+            })
 
-        socket.on("data-response", data => {
-            socket.to(sessionID).broadcast.emit("user-data-response", data)
-        })
+            socket.on("disconnect", () => {
+                socket.to(sessionID).broadcast.emit("user-disconnected", userID)
+            })
+
+
+            socket.on("request-user-data", data => {
+                socket.to(sessionID).broadcast.emit("data-requested", data)
+            })
+
+            socket.on("data-response", data => {
+                socket.to(sessionID).broadcast.emit("user-data-response", data)
+            })
+        }
     })
 })
 
